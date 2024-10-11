e6810d4fd6283b610ba42723db3b3778808b8731
Deftu <me@deftu.dev>
2024-10-11 14:53:18 +0200

    Implement stats route

--- src/main/kotlin/one/devos/nautical/exposeplayers/plugins/Routing.kt
+++ src/main/kotlin/one/devos/nautical/exposeplayers/plugins/Routing.kt
@@ -7,11 +7,25 @@ import io.ktor.server.routing.*
 import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
 import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.network.chat.Component
 import net.minecraft.server.MinecraftServer
+import net.minecraft.stats.StatType
 import net.minecraft.stats.Stats
 import one.devos.nautical.exposeplayers.utils.UUIDSerializer
 import one.devos.nautical.exposeplayers.utils.getPlayerStatsByUuid
 import java.util.UUID
+
+private val ITEM_STATS = listOf(
+    Stats.ITEM_USED,
+    Stats.ITEM_BROKEN,
+    Stats.ITEM_CRAFTED,
+    Stats.ITEM_PICKED_UP,
+    Stats.ITEM_DROPPED
+)
+
+private val BLOCK_STATS = listOf(
+    Stats.BLOCK_MINED,
+)
 
 fun Application.configureRouting(server: MinecraftServer) {
     routing {
@@ -41,12 +55,44 @@ fun Application.configureRouting(server: MinecraftServer) {
                 return@get
             }
 
-            val statsCounter = server.playerList.getPlayerStatsByUuid(playerUuid, playerName)
-//            call.respond(BuiltInRegistries.STAT_TYPE.map { statisticType -> {
-//                statisticType.flatMap
-//            })
+            val statList = mutableListOf<PlayerStatistic>()
 
-            
+            val statsCounter = server.playerList.getPlayerStatsByUuid(playerUuid, playerName)
+
+            for (resourceLocation in Stats.CUSTOM) {
+                statList.add(PlayerCustomStatistic(
+                    Component.translatable(resourceLocation.value.toLanguageKey("stat")).string,
+                    statsCounter.getValue(resourceLocation)
+                ))
+            }
+
+            for (item in BuiltInRegistries.ITEM) {
+                val types = mutableMapOf<ItemStatisticType, Int>()
+
+                for (statisticType in ITEM_STATS) {
+                    val value = statsCounter.getValue(statisticType.get(item))
+                    if (value != 0) {
+                        types[ItemStatisticType.from(statisticType)] = value
+                    }
+                }
+
+                statList.add(PlayerItemsStatistic(Component.translatable(item.descriptionId).string, types))
+            }
+
+            for (block in BuiltInRegistries.BLOCK) {
+                val types = mutableMapOf<BlockStatisticType, Int>()
+
+                for (statisticType in BLOCK_STATS) {
+                    val value = statsCounter.getValue(statisticType.get(block))
+                    if (value != 0) {
+                        types[BlockStatisticType.from(statisticType)] = value
+                    }
+                }
+
+                statList.add(PlayerBlocksStatistic(Component.translatable(block.descriptionId).string, types))
+            }
+
+            call.respond(statList)
         }
     }
 }
@@ -64,7 +110,51 @@ private data class PlayersEndpointResponse(
 )
 
 @Serializable
-private data class PlayerStatistic(
-    val displayName: String,
-    val value: Any
-)
+private sealed interface PlayerStatistic {
+    val displayName: String
+    @Contextual val value: Any
+}
+
+@Serializable
+private class PlayerCustomStatistic(
+    override val displayName: String,
+    @Contextual override val value: Any
+) : PlayerStatistic
+
+@Serializable
+private class PlayerItemsStatistic(
+    override val displayName: String,
+    @Contextual override val value: Map<ItemStatisticType, Int>,
+) : PlayerStatistic
+
+@Serializable
+private enum class ItemStatisticType(private val value: StatType<*>) {
+    USED(Stats.ITEM_USED),
+    BROKEN(Stats.ITEM_BROKEN),
+    CRAFTED(Stats.ITEM_CRAFTED),
+    PICKED_UP(Stats.ITEM_PICKED_UP),
+    DROPPED(Stats.ITEM_DROPPED);
+
+    companion object {
+        fun from(statisticType: StatType<*>): ItemStatisticType {
+            return entries.first { it.value == statisticType }
+        }
+    }
+}
+
+@Serializable
+private class PlayerBlocksStatistic(
+    override val displayName: String,
+    @Contextual override val value: Map<BlockStatisticType, Int>,
+) : PlayerStatistic
+
+@Serializable
+private enum class BlockStatisticType(private val value: StatType<*>) {
+    MINED(Stats.BLOCK_MINED);
+
+    companion object {
+        fun from(statisticType: StatType<*>): BlockStatisticType {
+            return entries.first { it.value == statisticType }
+        }
+    }
+}
