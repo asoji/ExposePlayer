720b5a998056ab44f0dcdf16608f0c478da5a1f9
Asoji <asoji-git@hiitsdevin.mozmail.com>
2024-10-14 22:35:49 -0700

    🧱 Netty -> Jetty, singleplayer works now

--- src/main/kotlin/one/devos/nautical/exposer/Exposer.kt
+++ src/main/kotlin/one/devos/nautical/exposer/Exposer.kt
@@ -3,7 +3,7 @@ package one.devos.nautical.exposer
 import gay.asoji.fmw.FMW
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
-import io.ktor.server.netty.*
+import io.ktor.server.jetty.jakarta.*
 import io.ktor.server.plugins.compression.*
 import io.ktor.server.routing.*
 import net.fabricmc.api.ModInitializer
@@ -18,14 +18,14 @@ object Exposer : ModInitializer {
     val LOGGER: Logger = LoggerFactory.getLogger(MOD_ID)
     val MOD_NAME: String = FMW.getName(MOD_ID)
 
-    private var server: EmbeddedServer<NettyApplicationEngine, NettyApplicationEngine.Configuration>? = null
+    private var server: EmbeddedServer<JettyApplicationEngine, JettyApplicationEngineBase.Configuration>? = null
 
     override fun onInitialize() {
         LOGGER.info("[${MOD_NAME}] Starting up ExposePlayers")
 
         ServerLifecycleEvents.SERVER_STARTED.register { server ->
             this.server?.stop()
-            this.server = embeddedServer(Netty, port = 64589, host = "0.0.0.0", module = {
+            this.server = embeddedServer(Jetty, port = 64589, host = "0.0.0.0", module = {
                 install(IgnoreTrailingSlash)
                 install(Compression)
 
